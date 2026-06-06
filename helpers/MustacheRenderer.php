fa63ccf279eece6f57d13c7b88adcdc1002f843d
Iván Chimenti <ichimenti@alumno.unlam.edu.ar>
2026-06-06 02:26:08 -0300

    fix: se corrigen errores para poder ver en funcionamiento el proyecto

--- helpers/MustacheRenderer.php
+++ helpers/MustacheRenderer.php
@@ -1,12 +1,11 @@
 <?php
 
-require_once(__DIR__ . '/../vendor/mustache/src/Mustache/Autoloader.php');
+require_once __DIR__ . '/../vendor/autoload.php';
 
 class MustacheRenderer {
     private $mustache;
 
     public function __construct($viewsFolder) {
-        Mustache_Autoloader::register();
         $this->mustache = new Mustache_Engine([
             'loader'          => new Mustache_Loader_FilesystemLoader($viewsFolder),
             'partials_loader'  => new Mustache_Loader_FilesystemLoader($viewsFolder),
